f9dbde61addee5f12bfff6bcf4fb17f24319e926
labanything <45777202+labanything@users.noreply.github.com>
2019-07-11 19:50:25 +0900

    1

---
+++ reinforcement-1-grid-world/1-policy-iteration/policy_iteration.py
@@ -0,0 +1,131 @@
+# -*- coding: utf-8 -*-
+import random
+from environment import GraphicDisplay, Env
+
+class PolicyIteration:
+    def __init__(self, env):
+        # 환경에 대한 객체 선언
+        self.env = env
+        # 가치함수를 2차원 리스트로 초기화
+        self.value_table = [[0.0] * env.width for _ in range(env.height)]
+        # 상 하 좌 우 동일한 확률로 정책 초기화
+        self.policy_table = [[[0.25, 0.25, 0.25, 0.25]] * env.width
+                                    for _ in range(env.height)]
+        # 마침 상태의 설정
+        self.policy_table[2][2] = []
+        # 감가율
+        self.discount_factor = 0.9
+
+
+    #버튼을 눌렀을때 실행되는 함수1
+    #정책평가
+    def policy_evaluation(self):
+        # 다음 가치함수 초기화
+        next_value_table = [[0.00] * self.env.width
+                                    for _ in range(self.env.height)]
+
+        # 모든 상태에 대해서 벨만 기대방정식을 계산
+        for state in self.env.get_all_states():
+            value = 0.0
+            # 마침 상태의 가치 함수 = 0
+            if state == [2, 2]:
+                next_value_table[state[0]][state[1]] = value
+                continue
+
+            # 벨만 기대 방정식
+            for action in self.env.possible_actions:
+                next_state = self.env.state_after_action(state, action)
+                reward = self.env.get_reward(state, action)
+                next_value = self.get_value(next_state)
+                value += (self.get_policy(state)[action] *
+                          (reward + self.discount_factor * next_value))
+
+            next_value_table[state[0]][state[1]] = round(value, 2)
+
+        self.value_table = next_value_table
+
+    # 버튼을 눌렀을때 실행되는 함수2
+    # 현재 가치 함수에 대해서 탐욕 정책 발전
+    def policy_improvement(self):
+        next_policy = self.policy_table
+        for state in self.env.get_all_states():
+            if state == [2, 2]:
+                continue
+            value = -99999
+            max_index = []
+            # 반환할 정책 초기화
+            result = [0.0, 0.0, 0.0, 0.0]
+
+            # 모든 행동에 대해서 [보상 + (감가율 * 다음 상태 가치함수)] 계산
+            for index, action in enumerate(self.env.possible_actions):
+                next_state = self.env.state_after_action(state, action)
+                reward = self.env.get_reward(state, action)
+                next_value = self.get_value(next_state)
+                temp = reward + self.discount_factor * next_value
+
+                # 받을 보상이 최대인 행동의 index(최대가 복수라면 모두)를 추출
+                if temp == value:
+                    max_index.append(index)
+                elif temp > value:
+                    value = temp
+                    max_index.clear()
+                    max_index.append(index)
+
+            # 행동의 확률 계산
+            prob = 1 / len(max_index)
+
+            for index in max_index:
+                result[index] = prob
+
+            next_policy[state[0]][state[1]] = result
+
+        self.policy_table = next_policy
+
+    # 버튼을 눌렀을때 실행되는 함수3
+    # 특정 상태에서 정책에 따른 행동을 반환
+    def get_action(self, state):
+        # 0 ~ 1 사이의 값을 무작위로 추출
+        random_pick = random.randrange(100) / 100
+
+        policy = self.get_policy(state)
+        policy_sum = 0.0
+        # 정책에 담긴 행동 중에 무작위로 한 행동을 추출
+        for index, value in enumerate(policy):
+            policy_sum += value
+            if random_pick < policy_sum:
+                return index
+
+    # 상태에 따른 정책 반환
+    def get_policy(self, state):
+        if state == [2, 2]:
+            return 0.0
+        return self.policy_table[state[0]][state[1]]
+
+    # 가치 함수의 값을 반환
+    def get_value(self, state):
+        # 소숫점 둘째 자리까지만 계산
+        return round(self.value_table[state[0]][state[1]], 2)
+
+if __name__ == "__main__":
+    env = Env()
+    policy_iteration = PolicyIteration(env)
+    grid_world = GraphicDisplay(policy_iteration)
+    grid_world.mainloop()
+
+
+'''
+에이전트
+    초기화
+        환경객체 선언
+    정책평가
+    정책발전
+    행동(상태)
+    정책(상태)
+    가치함수(상태)
+메인
+    환경객체생성
+    정책이터레이션의 객체생성(환경)
+        에이전트가 환경에 접속할 수 있게하였다.
+    그리드월드의 객체생성(정책이터레이션)
+    그리드월드.메인루프 생성
+'''
